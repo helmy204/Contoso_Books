90727d962f79bc5a4df8721fb20f306ad9fe611a
helmy204 <hamadaelking204@hotmail.com>
2016-01-11 14:06:31 +0200

    Create entity classes

---
+++ ContosoBooks/src/ContosoBooks/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoBooks.Models
+{
+    public class Book
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+        public int AuthorID { get; set; }
+
+        // Navigation property
+        public Author Author { get; set; }
+    }
+}
